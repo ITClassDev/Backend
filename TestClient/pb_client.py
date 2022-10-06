1843f8818083d2b399b59d947a4ffc10272d8241
ret7020 <ret7020@gmail.com>
2022-10-06 20:16:35 +0300

    Answer as protobuffer

--- TestClient/pb_client.py
+++ TestClient/pb_client.py
@@ -3,8 +3,19 @@ import sys
 sys.path.append("../")
 import pbs.test_pb2 as TestBuffer
 
+def parse_data(data, buffer_type):
+    res_buffer = buffer_type()
+    res_buffer.ParseFromString(data)
+    return res_buffer
+
+
+
 if __name__ == "__main__":
     buffer = TestBuffer.TestData()
     buffer.a = 1024
     buffer_final = buffer.SerializeToString()
     data = requests.post("http://localhost:8080/pb/test", headers={'Content-Type': 'application/protobuf'}, data=buffer_final)
+    answer = data.content
+    print(answer)
+    parsed = parse_data(answer, TestBuffer.TestData)
+    print("Answer:", parsed)
--- endpoints/protobuf.py
+++ endpoints/protobuf.py
@@ -1,16 +1,27 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 import pbs.test_pb2 as TestBuffer
 import asyncio
 
 
+async def parse_data(data, buffer_type):
+    res_buffer = buffer_type()
+    res_buffer.ParseFromString(data)
+    return res_buffer
 
+async def create_answer(data, buffer_type):
+    
+    return Response(content=answer_buffer_final, media_type="application/protobuf")
 
 router = APIRouter()
+
+
+# Dev test
 @router.post("/test")
 async def test_proto_buffer(request: Request):
-    data = await request.body()
-    res_buffer = TestBuffer.TestData()
-    print(data)
-    res_buffer.ParseFromString(data)
-    print(res_buffer.a)
-
+    data = await parse_data(await request.body(), TestBuffer.TestData)
+    print("Recieve:", data)
+    answer_buffer = TestBuffer.TestData()
+    answer_buffer.a = 1024
+    answer_buffer_final = answer_buffer.SerializeToString()
+    print(answer_buffer_final)
+    return Response(content=answer_buffer_final, media_type="application/protobuf")
