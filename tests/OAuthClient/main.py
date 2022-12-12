c276c37ac22241bb64898a1ac13ab168a30c4999
ret7020 <ret7020@gmail.com>
2022-12-12 22:36:03 +0300

    test client

--- tests/OAuthClient/main.py
+++ tests/OAuthClient/main.py
@@ -1,10 +1,21 @@
-# Test OAuth(JWT SSO)
+# Test OAuth app using FastAPI
 
+from fastapi import FastAPI
+import uvicorn
 import requests
 
+def getOAuthedUser(api="http://localhost:8080/oauth/get_user"):
+    data = requests.get("")
 
-class Client:
-    pass
 
-class API:
-    pass
+app = FastAPI()
+
+
+@app.get("/login_with_shtp") # endpoint
+async def login_with_shtp(access_token: int):
+    print("New login with token:", access_token)
+    return {"data_from_shtp": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=3232, host="localhost", reload=True)
