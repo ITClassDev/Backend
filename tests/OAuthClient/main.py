85dccd934c37dfdb8f2f81f56b6286f5ea6f5f16
ret7020 <ret7020@gmail.com>
2022-12-13 23:01:36 +0300

    Full example of OAuth

--- tests/OAuthClient/main.py
+++ tests/OAuthClient/main.py
@@ -3,7 +3,22 @@
 from fastapi import FastAPI
 import uvicorn
 import requests
+from typing import List, Dict
 
+# Fake database as json to store shtp_id and real user id
+# Real user id's are differ from shtp to demonstrate OAuth scheme
+FAKE_DB_USERS = [
+    {"id": 123, "name": "Stephan Zhdanov", "shtp_id": 1},
+    {"id": 232, "name": "Test user", "shtp_id": 2}
+]
+
+# Like sql select function (utility)
+async def findUserByShtp(db: List[Dict], shtp_id: int):
+    for user in db:
+        if user["shtp_id"] == shtp_id:
+            return user
+
+# Make request to shtp api to get verified answer about user id
 async def getOAuthedUser(token: str, api: str="http://localhost:8080/oauth/get_user") -> int:
     data = requests.get(f"{api}/{token}").json()
     if data["status"]:
@@ -11,20 +26,24 @@ async def getOAuthedUser(token: str, api: str="http://localhost:8080/oauth/get_u
     else: # invalid token
         return -1
 
+# init fastapi app
+app = FastAPI() 
 
-app = FastAPI() # init fastapi app
-
-
-@app.get("/login_with_shtp") # endpoint to login (redirect url)
+# endpoint to login (redirect url)
+@app.get("/login_with_shtp")
 async def login_with_shtp(access_token: str): # get param -> access token
     print("[DEBUG] New login with token:", access_token)
+    status = "FAIL"
     shtp_user_id = await getOAuthedUser(token=access_token)
     if shtp_user_id != -1:
         print("[DEBUG] Confirmed user id:", shtp_user_id)
+        real_user_id = await findUserByShtp(FAKE_DB_USERS, shtp_user_id)
+        print("[DEBUG] Real user linked to this shtp id:", real_user_id)
+        status = "OK"
     else:
         print("[DEBUG] Can't get user id; invalid token")
     
-    return {"data_from_shtp": "Hello World"}
+    return {"oauth": status}
 
 
 if __name__ == "__main__":
