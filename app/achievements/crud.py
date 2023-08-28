c0b2da811a563717e30b691b8430ee912ae529a9
ret7020 <ret7020@gmail.com>
2023-08-28 19:40:31 +0300

    Points and notifications on achievement reject/accept

--- app/achievements/crud.py
+++ app/achievements/crud.py
@@ -7,6 +7,8 @@ from sqlalchemy import select, update, delete
 from typing import List
 from app.achievements.models import Achievement
 from app.achievements.schemas import AchievementCreate, AchievementModerate
+from app.notifications.schemas import NotificationCreate
+from app.notifications.crud import NotificationsCRUD
 from app.users.models import User
 import uuid as uuid_pkg
 import datetime
@@ -40,15 +42,28 @@ class AchievementsCRUD:
 
 
     async def moderate(self, achievement_moderate: AchievementModerate, moder_uuid: uuid_pkg.UUID) -> None: 
+        query_sel = select(Achievement, User).where(Achievement.uuid == achievement_moderate.uuid, User.uuid == Achievement.toUser)
+        results = await self.session.execute(query_sel)
+        results = results.fetchall()
+        achievement = results[0][0]
+        student = results[0][1]
+        notification_crud = NotificationsCRUD(self.session)
+        
         if not achievement_moderate.status: # Reject -> delete
             query = delete(Achievement).where(Achievement.uuid == achievement_moderate.uuid)
-        else:
+            await notification_crud.create_single(NotificationCreate(toUser=student.uuid, type=1, data={"name": achievement.title}))
+        else: # Accept -> add points
             query = update(Achievement).where(Achievement.uuid == achievement_moderate.uuid).values(
                 acceptedBy=moder_uuid,
                 acceptedAt=datetime.datetime.utcnow(),
                 points=achievement_moderate.points
             )
-
+            student.rating += achievement_moderate.points
+            await notification_crud.create_single(NotificationCreate(toUser=student.uuid, type=0, data={"name": achievement.title, "points": achievement_moderate.points}))
+            # query = update(User).where(User.uuid == user_uuid).values(rating=User.rating += 1)
+            
+        # Commit moderate status
+        # TODO: uncomment
         await self.session.execute(query)
         await self.session.commit()
         
